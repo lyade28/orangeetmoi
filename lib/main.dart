04332636d57c15baa72c4848a934cbd3ec0c322b
lyade28 <libasseyadee@gmail.com>
2022-08-25 15:39:30 +0000

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,18 +29,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  int _currentIndex = 0;
+  final tabs = [
+    Center(
+      child: Text('2'),
+    ),
+    Center(
+      child: Text('3'),
+    ),
+    Center(
+      child: Text('4'),
+    ),
+    Center(
+      child: Text('5'),
+    ),
+    Center(
+      child: Text('5'),
+    ),
+    Center(
+      child: Text('6'),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('data'),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'fef'),
-        BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'fejf'),
-        BottomNavigationBarItem(icon: Icon(Icons.abc_sharp), label: 'fejf'),
-      ]),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.home,
+                color: Colors.orange,
+              ),
+              label: 'fef'),
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.chartPie,
+                color: Colors.orange,
+              ),
+              label: 'fejf'),
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.exchange,
+                color: Colors.orange,
+              ),
+              label: 'fejf'),
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.basketShopping,
+                color: Colors.orange,
+              ),
+              label: 'fejf'),
+          BottomNavigationBarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.handshakeSimple,
+                color: Colors.orange,
+              ),
+              label: 'fejf'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
